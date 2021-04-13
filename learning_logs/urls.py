e70ac1664b9ad71b6f163c837ec487b8eadeef79
Windstarry <13782656673@163.com>
2021-04-13 17:22:43 +0800

    v1.0学习笔记初始功能

---
+++ learning_logs/urls.py
@@ -0,0 +1,10 @@
+'''定义learning_log的urls模式'''
+from django.conf.urls import url
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    url(r'^$',views.index,name = 'index'),
+    url(r'^topics/$',views.topics,name = 'topics'),
+    url(r'^topics/(?P<topic_id>\d+)$',views.topic,name = 'topic')
+]
