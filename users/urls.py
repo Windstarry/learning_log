99163d485d7743565134e038bd6f2d68d52a8fa9
Windstarry <13782656673@163.com>
2021-04-16 17:18:25 +0800

    增加用户注册功能

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(template_name='users/login.html'),name = 'login'),
     url(r'^logout/$',views.logout_views,name='logout'),
+    url(r'^register/$',views.register,name='register')
 ]
